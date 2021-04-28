157b9426e1a103b2fe6ba05f982ec5f59efe5cfc
JewelryMan64 <srithpong.udo@student.mahidol.ac.th>
2021-04-28 20:27:51 +0700

    Finish encryption and decryption

---
+++ lib/const.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+final themeColor = Color(0xfff795548);
+final primaryColor = Color(0xffFFFFFF);
+final greyColor = Color(0xffaeaeae);
+final greyColor2 = Color(0xff5D4037);
+final textboxColor = Color(0xffA1887F);
+int caesarKey = 0;
